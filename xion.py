a3f3c4c48c0fa2f39f816065edfdbd799a84f82e
Ben <macdonaldbx09@live.ca>
2024-07-17 22:27:43 -0300

    Xion is now functioning in discord server, only responds to test message

--- xion.py
+++ xion.py
@@ -7,18 +7,22 @@ from dotenv import load_dotenv
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-client = discord.Client()
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
-    print(f'{client.user} has entered the Round Room. Praise Kingdom Hearts.')
+    onlineMessage = f'{client.user} has entered the Round Room. Praise Kingdom Hearts.'
+    print(onlineMessage)
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
     
-    if message.content == "I'll use this instead!":
+    if message.content == 'I\'ll use this instead!':
         response = 'Roxas, that\'s a stick.'
         await message.channel.send(response)
 
